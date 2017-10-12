6ffa0e13cab257efa8580a3853b01451997d750c
Brent Schroder <schroder.brent@gmail.com>
2017-10-12 16:10:21 -0600

    Random Colors Checkerboard

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
+//   var numbertiles=81;
+//   for (var i=0;i<numbertiles;i++){
+//     let tile = document.createElement('div');    Master
+//     tile.style.width = '11.1%';
+//     tile.style.float = 'left';
+//     tile.style.paddingBottom= '11.1%';
+//     if(i%2===0){
+//     tile.style.backgroundColor='red';
+//     }else{
+//     tile.style.backgroundColor='black';
+//     }
+//     document.body.appendChild(tile);
+//   }
+//})
+
   var numbertiles=81;
-  for (var i=0;i<numbertiles;i++){
+  for (let i=0;i<numbertiles;i++){
     let tile = document.createElement('div');
     tile.style.width = '11.1%';
     tile.style.float = 'left';
     tile.style.paddingBottom= '11.1%';
-    if(i%2===0){
-    tile.style.backgroundColor='red';
-    }else{
-    tile.style.backgroundColor='black';
+    var letters = '0123456789ABCDEF';
+    var randcolor = '#';
+    for (let i = 0; i < 6; i++) {
+      randcolor += letters[Math.floor(Math.random() * 16)];
     }
+    console.log(randcolor)
+    tile.style.backgroundColor= ''+randcolor;
     document.body.appendChild(tile);
   }
 })
