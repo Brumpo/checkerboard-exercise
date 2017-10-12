7508de97e360d5afa2ebcfaff3c5b845e83e4be3
Brent Schroder <schroder.brent@gmail.com>
2017-10-12 16:48:33 -0600

    Gradient checkerboard

--- script.js
+++ script.js
@@ -14,19 +14,41 @@ document.addEventListener('DOMContentLoaded', function () {
 //   }
 //})
 
-  var numbertiles=81;
+//   var numbertiles=81;
+//   for (let i=0;i<numbertiles;i++){
+//     let tile = document.createElement('div');
+//     tile.style.width = '11.1%';
+//     tile.style.float = 'left';
+//     tile.style.paddingBottom= '11.1%';
+//     var letters = '0123456789ABCDEF';      Randomcolors
+//     var randcolor = '#';
+//     for (let i = 0; i < 6; i++) {
+//       randcolor += letters[Math.floor(Math.random() * 16)];
+//     }
+//     console.log(randcolor)
+//     tile.style.backgroundColor= ''+randcolor;
+//     document.body.appendChild(tile);
+//   }
+// })
+var numbertiles=81;
   for (let i=0;i<numbertiles;i++){
     let tile = document.createElement('div');
     tile.style.width = '11.1%';
     tile.style.float = 'left';
     tile.style.paddingBottom= '11.1%';
-    var letters = '0123456789ABCDEF';
-    var randcolor = '#';
-    for (let i = 0; i < 6; i++) {
-      randcolor += letters[Math.floor(Math.random() * 16)];
-    }
-    console.log(randcolor)
-    tile.style.backgroundColor= ''+randcolor;
+    var startcolor= randomcolor();
+    var endcolor= randomcolor();
+//     console.log(randcolor)
+    tile.style.backgroundImage= `linear-gradient(45deg, ${startcolor}, ${endcolor})`;
+    console.log(tile.style.backgroundImage);
     document.body.appendChild(tile);
   }
 })
+function randomcolor(){
+  var letters = '0123456789ABCDEF';
+  var randcolor = '#';
+  for (let i = 0; i < 6; i++) {
+    randcolor += letters[Math.floor(Math.random() * 16)];
+  }
+  return randcolor
+}
