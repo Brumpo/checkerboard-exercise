5dc62ca9aa4c005e1bad8e238e05cdbe42200eda
Brent Schroder <schroder.brent@gmail.com>
2017-10-12 15:49:40 -0600

    Working Red and Black Checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Your JS goes here
+document.addEventListener('DOMContentLoaded', function () {
+  var numbertiles=81;
+  for (var i=0;i<numbertiles;i++){
+    let tile = document.createElement('div');
+    tile.style.width = '11.1%';
+    tile.style.float = 'left';
+    tile.style.paddingBottom= '11.1%';
+    if(i%2===0){
+    tile.style.backgroundColor='red';
+    }else{
+    tile.style.backgroundColor='black';
+    }
+    document.body.appendChild(tile);
+  }
+})
