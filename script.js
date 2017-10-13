4691848fc5e2c919daabc7bb9e91110e26ea407a
Brent Schroder <schroder.brent@gmail.com>
2017-10-12 18:58:32 -0600

    Flashing Checkerboard

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+// document.addEventListener('DOMContentLoaded', function () {
 //   var numbertiles=81;
 //   for (var i=0;i<numbertiles;i++){
 //     let tile = document.createElement('div');    Master
@@ -30,25 +30,46 @@ document.addEventListener('DOMContentLoaded', function () {
 //     document.body.appendChild(tile);
 //   }
 // })
-var numbertiles=81;
+// var numbertiles=81;
+//   for (let i=0;i<numbertiles;i++){
+//     let tile = document.createElement('div');
+//     tile.style.width = '11.1%';
+//     tile.style.float = 'left';
+//     tile.style.paddingBottom= '11.1%';
+//     var startcolor= randomcolor();
+//     var endcolor= randomcolor();
+// //     console.log(randcolor)
+//     tile.style.backgroundImage= `linear-gradient(45deg, ${startcolor}, ${endcolor})`;
+//     console.log(tile.style.backgroundImage);
+//     document.body.appendChild(tile);
+//   }
+// })
+  var numbertiles=81;
   for (let i=0;i<numbertiles;i++){
     let tile = document.createElement('div');
     tile.style.width = '11.1%';
     tile.style.float = 'left';
     tile.style.paddingBottom= '11.1%';
-    var startcolor= randomcolor();
-    var endcolor= randomcolor();
+    var randcolor= randomcolor();
 //     console.log(randcolor)
-    tile.style.backgroundImage= `linear-gradient(45deg, ${startcolor}, ${endcolor})`;
-    console.log(tile.style.backgroundImage);
+    tile.style.backgroundColor=''+randcolor;
     document.body.appendChild(tile);
   }
-})
+  document.addEventListener('DOMContentLoaded', function (){
+    let tile = document.getElementsByTagName('div');
+    for(let i=0;i<tile.length;i++){
+      window.setInterval(function(){
+        tile[i].style.backgroundColor= ''+randomcolor() ? ''+randomcolor() : ''+randomcolor();
+      },2000)
+    }
+  })
+
+
 function randomcolor(){
   var letters = '0123456789ABCDEF';
   var randcolor = '#';
   for (let i = 0; i < 6; i++) {
     randcolor += letters[Math.floor(Math.random() * 16)];
   }
-  return randcolor
-}
+  return randcolor;
+}//
